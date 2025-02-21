9a5149d154e8a10bb720457634debc03beb9b2be
eepasion <elias.pasion@pragma.com.co>
2025-02-21 11:09:42 -0500

    feat: add archetype and rename to technology

---
+++ src/main/java/com/example/technology/TechnologyMfApplication.java
@@ -0,0 +1,13 @@
+package com.example.technology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnologyMfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnologyMfApplication.class, args);
+	}
+
+}
