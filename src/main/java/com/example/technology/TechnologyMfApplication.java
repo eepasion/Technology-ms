168b4a0608854c8f4ea4d28ba71b2b2b3ddf6b46
eepasion <elias.pasion@pragma.com.co>
2025-02-26 08:15:58 -0500

    feat: add swagger title

--- src/main/java/com/example/technology/TechnologyMfApplication.java
+++ src/main/java/com/example/technology/TechnologyMfApplication.java
@@ -1,13 +1,16 @@
 package com.example.technology;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(info = @Info(title = "Tecnologia Microservicio", version = "1.0", description = "Microservicio para gestionar las tecnologias"))
 @SpringBootApplication
 public class TechnologyMfApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TechnologyMfApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TechnologyMfApplication.class, args);
+    }
 
 }
